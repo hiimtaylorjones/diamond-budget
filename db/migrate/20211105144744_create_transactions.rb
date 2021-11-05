611f8a0a5f8118ffe328ac002b5ecb39fb5fdff7
hiimtaylorjones <taylorjonessoftware@gmail.com>
2021-11-05 10:57:50 -0400

    creating budget and transaction models

---
+++ db/migrate/20211105144744_create_transactions.rb
@@ -0,0 +1,13 @@
+class CreateTransactions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :transactions do |t|
+      t.belongs_to :budget
+      t.string :payee
+      t.date :execute_date
+      t.integer :value
+      t.string :notes
+      t.boolean :cleared
+      t.timestamps
+    end
+  end
+end
